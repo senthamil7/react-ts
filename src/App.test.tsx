092a586aef44bf1ccccf31ae8450fda85c39f7cf
Senthamil selvan <senthamilselvan@Senthamils-MacBook-Pro.local>
2022-02-18 10:00:04 +0530

    initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { Header } from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Header />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export const Header = () => {
+  
+const [count, setCounter] = useState(0);
+const state = { count: 0 };
+const onIncrement = (): any => {
+  console.log(state);
+     setCounter(count+1);
+};
+
+const decrement = (): any => {
+  console.log(state);
+  setCounter(count -1);
+
+};
+  return(
+    <div>
+      <span>
+        Increment: {count}
+      </span>
+      <button type="button" onClick={onIncrement}>
+        {`Increment`}
+      </button>
     </div>
   );
 }
 
-export default App;
+
